4165d13da3c81aa791e289c966b6db6ed1bec080
Amrita mishra <amritamishraxa06@gmail.com>
2025-06-07 13:44:00 +0530

    changes:Homescreen done

--- src/layouts/MainLayout.tsx
+++ src/layouts/MainLayout.tsx
@@ -1,5 +1,5 @@
 import Container from "@/components/Container"
-import Footer from "@/components/Footer"
+import {Footer} from "@/components/Footer"
 import Header from "@/components/Header"
 import { Outlet } from "react-router-dom"
 
--- src/layouts/PublicLayout.tsx
+++ src/layouts/PublicLayout.tsx
@@ -1,4 +1,5 @@
-import Footer from "@/components/Footer"
+
+import { Footer } from "@/components/Footer"
 import Header from "@/components/Header"
 import AuthHandler from "@/handlers/AuthHandler"
 import { Outlet } from "react-router-dom"
